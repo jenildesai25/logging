31a1b4650f8c8cd516b590228064a9f7a92932c9
Jenil Desai <jenil.desai25@gmail.com>
2018-06-27 21:53:02 -0500

    Create closeButton.py

---
+++ closeButton.py
@@ -0,0 +1,57 @@
+import sys
+from PyQt5.QtWidgets import (
+    QApplication, QWidget, QToolTip, QPushButton, QMessageBox)
+from PyQt5.QtCore import QCoreApplication, Qt
+
+
+class Window(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        btn = QPushButton()
+        # btn.setToolTip("Close Application")
+        # btn.clicked.connect(QCoreApplication.instance().quit)
+        # instead of above button signal, try to call closeEvent method below
+        btn.clicked.connect(self.closeEvent)
+
+        btn.resize(btn.sizeHint())
+        btn.move(410, 118)
+        self.setGeometry(30, 450, 500, 150)
+        self.setWindowTitle("Terminator")
+        self.show()
+
+    def closeEvent(self, event):
+        """Generate 'question' dialog on clicking 'X' button in title bar.
+
+        Reimplement the closeEvent() event handler to include a 'Question'
+        dialog with options on how to proceed - Save, Close, Cancel buttons
+        """
+        reply = QMessageBox.question(
+            self, "Message",
+            "Are you sure you want to quit? Any unsaved work will be lost.",
+            QMessageBox.Yes | QMessageBox.No,
+            QMessageBox.Yes)
+        # int(reply)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def keyPressEvent(self, event):
+        """Close application from escape key.
+
+        results in QMessageBox dialog from closeEvent, good but how/why?
+        """
+        if event.key() == Qt.Key_Escape:
+            self.close()
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    w = Window()
+    sys.exit(app.exec_())
